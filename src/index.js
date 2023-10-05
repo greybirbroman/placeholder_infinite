305e56b09e1f94e02da3c6f0f05063533cbc1b16
greybirbroman <98167513+greybirbroman@users.noreply.github.com>
2023-10-05 19:21:54 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './features/store';
-import App from './App';
-import { extendedApiSlice } from './features/posts/postsSlice';
-
-store.dispatch(extendedApiSlice.endpoints.getPosts.initiate())
+import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
